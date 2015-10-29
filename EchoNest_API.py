aeae0f8f54cd5f2cc86e75f268577349577e4316
Jason Adams <jca4hq@virginia.edu>
2015-10-29 19:24:24 -0400

    Changed so we don't make as many calls and setting up loop to get all data at once

--- EchoNest_API.py
+++ EchoNest_API.py
@@ -7,25 +7,32 @@ import time
 
 
 def get_factors(Artist, Recording):
-        songid_url = 'http://developer.echonest.com/api/v4/song/search?api_key=QOEISO4ZCNSPLKQOQ&artist=' + Artist + '&title=' + Recording
+        songid_url = 'http://developer.echonest.com/api/v4/song/search?api_key=QOEISO4ZCNSPLKQOQ&artist=' \
+                      + Artist + '&title=' + Recording + '&bucket=audio_summary'
         songid_dict = json.loads(bytes.decode(urllib.request.urlopen(songid_url).read()))
-        song_id = songid_dict['response']['songs'][0]['id']
+        song_id = songid_dict['response']['songs'][0]['audio_summary']
 
-        factors_url = 'http://developer.echonest.com/api/v4/song/profile?api_key=QOEISO4ZCNSPLKQOQ&id=' + song_id + '&bucket=audio_summary'
-        factors_dict = json.loads(bytes.decode(urllib.request.urlopen(factors_url).read()))
-        factors = factors_dict['response']['songs'][0]['audio_summary']
-        time.sleep(10)
-        return(factors)
+        return(song_id)
 
 unique = pd.read_csv('unique_artists_songs.csv', encoding='utf-8')
 unique['oneartist'] = unique['oneartist'].map(lambda x: x.replace(' ', '+'))
 unique['Recording1'] = unique['Recording1'].map(lambda x: x.replace(' ', '+'))
 
-print(unique.head(10))
+#print(unique.head(10))
 attr = []
-for i in unique.head(20).index:
-        attr.append(get_factors(unique.ix[i].oneartist, unique.ix[i].Recording1))
+for i in range(0, 40, 20):
+    for i in unique[i:i+20].index:
+        try:
+            attr.append(get_factors(unique.ix[i].oneartist, unique.ix[i].Recording1))
+        except IndexError:
+            attr.append('could_not_find')
+        except UnicodeEncodeError:
+            attr.append('unicode_error')
+        else:
+            continue
+    time.sleep(61)
 #print(get_factors(unique.ix[19].oneartist, unique.ix[19].Recording1))
 
 #print(int(unique.ix[4].pos))
-#print(attr)
+print(attr[1])
+#print(unique[5:7].index)
